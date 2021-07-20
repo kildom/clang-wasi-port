709913e9d8250a1efb257d07be66ae4d1ec2bd99
kildom <kildom@users.noreply.github.com>
2021-07-20 11:57:46 +0200

    clang-port: Add CI

--- build-wasi-sdk-new.sh
+++ build-wasi-sdk-new.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+LLVM_BASE_COMMIT=176249bd6732a8044d457092ed932768724a6f06
+
 if [ ! -d wasi-sdk ]
 then
     mkdir wasi-sdk
@@ -9,7 +11,18 @@ fi
 rm -Rf wasi-sdk-new
 cp -R wasi-sdk wasi-sdk-new
 cd wasi-sdk-src/
-make build/llvm.BUILT build/wasi-libc.BUILT
+if [ ! -d build/llvm/bin ]
+then
+    cd src/llvm-project
+    LLVM_OLD_COMMIT=`git rev-parse HEAD`
+    git checkout $LLVM_BASE_COMMIT
+    cd ../..
+    make build/llvm.BUILT
+    cd src/llvm-project
+    git checkout $LLVM_OLD_COMMIT
+    cd ../..
+fi
+make build/wasi-libc.BUILT
 cp -R build/install/opt/wasi-sdk/* ../wasi-sdk-new
 cp -R ../pthread_stub/include/* ../wasi-sdk-new/share/wasi-sysroot/include
 cp -R ../wasi-sdk-new/* build/install/opt/wasi-sdk
